62184a70ccda8724cb8c941819fa87c522e8cf85
Ahmad Ayash <ahmed.ayyash@gmail.com>
2018-01-13 22:47:33 +0100

    Add UI Validation for the rest of the fields

--- ExpediaInterview/ViewModel/QueryParametersViewModel.cs
+++ ExpediaInterview/ViewModel/QueryParametersViewModel.cs
@@ -4,9 +4,45 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using System.Reflection;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace ExpediaInterview.ViewModel
 {
+
+    public class DateStringAttribute : ValidationAttribute
+    {
+        private string _dateFormat;
+
+        public DateStringAttribute(string dateFormat)
+        {
+            _dateFormat = dateFormat;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return ValidationResult.Success;
+
+            }
+
+            try
+            {
+                DateTime dt = DateTime.ParseExact(value.ToString(), _dateFormat, System.Globalization.CultureInfo.InvariantCulture);
+
+                return ValidationResult.Success;
+            }
+            catch(ArgumentNullException e)
+            {
+                return new ValidationResult(e.Message);
+            }
+            catch(FormatException e)
+            {
+                return new ValidationResult(e.Message);
+            }
+        }
+    }
+
     public class QueryParametersViewModel
     {
         [Required]
@@ -21,17 +57,48 @@ namespace ExpediaInterview.ViewModel
         [StringLength(60, MinimumLength = 3)]
         public string Uid { get; set; }
 
+        [StringLength(60, MinimumLength = 3)]
+        [Display(Name = "Product Type")]
         public string ProductType { get; set; }
-        public string DestinationName { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Display(Name = "Destination City")]
         public string DestinationCity { get; set; }
+
+        [DateStringAttribute("yyyy-MM-dd")]
+        [Display(Name = "Minimum Trip Start Date")]
         public string MinTripStartDate { get; set; }
+
+        [DateStringAttribute("yyyy-MM-dd")]
+        [Display(Name = "Maximum Trip Start Date")]
         public string MaxTripStartDate { get; set; }
+
+        [Range(1, 100, ErrorMessage = "Stay must be a number between 1 and 100 days")]
+        [Display(Name = "Length Of Stay")]
         public string LengthOfStay { get; set; }
+
+        [Range(0.0, 5.0, ErrorMessage = "Rating must be a number betweein 0.0 and 5.0")]
+        [Display(Name = "Minimum Star Rating")]
         public string MinStarRating { get; set; }
+
+        [Range(0.0, 5.0, ErrorMessage = "Rating must be a number betweein 0.0 and 5.0")]
+        [Display(Name = "Max Star Rating")]
         public string MaxStarRating { get; set; }
+
+        [Range(0.0, 5.0, ErrorMessage = "Rating must be a number betweein 0.0 and 5.0")]
+        [Display(Name = "Min Guest Rating")]
         public string MinGuestRating { get; set; }
+
+        [Range(0.0, 5.0, ErrorMessage = "Rating must be a number betweein 0.0 and 5.0")]
+        [Display(Name = "Max Guest Rating")]
         public string MaxGuestRating { get; set; }
+
+        [Range(0.0, 5.0, ErrorMessage = "Rating must be a number betweein 0.0 and 5.0")]
+        [Display(Name = "Min Total Rating")]
         public string MinTotalRate { get; set; }
+
+        [Range(0.0, 5.0, ErrorMessage = "Rating must be a number betweein 0.0 and 5.0")]
+        [Display(Name = "Max Total Rating")]
         public string MaxTotalRate { get; set; }
 
 
